3e5de3856a64485a033f1088e6ca41ccf01f270d
Florian Feigenbutz <florian.feigenbutz@moovel.com>
2018-05-18 10:46:04 +0200

    Preselecting currently selected shelf

--- src/Book.js
+++ src/Book.js
@@ -14,7 +14,8 @@ class Book extends Component {
         <div className="book-top">
           <div className="book-cover" style={ coverStyle }></div>
           <div className="book-shelf-changer">
-            <select>
+            <select
+              defaultValue={book.shelf}>
               <option value="none" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
