e1c7c3affc32af472dd63da1e390c4a5c6cff877
Florian Feigenbutz <florian.feigenbutz@moovel.com>
2018-05-18 11:52:41 +0200

    Fixed preselection of not yet shelved books

--- src/Book.js
+++ src/Book.js
@@ -24,7 +24,7 @@ class Book extends Component {
           <div className="book-shelf-changer">
             <select
               onChange={ this.onShelfChange }
-              defaultValue={ book.shelf }>
+              defaultValue={ book.shelf || 'none' }>
               <option value="none" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
