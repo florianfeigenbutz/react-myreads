d8997f6c7e16deba6a4032865db3462cf1297158
Florian Feigenbutz <florian.feigenbutz@moovel.com>
2018-05-18 12:08:05 +0200

    Displaying 'None' as shelf for books in search results that are not yet added.

--- src/Book.js
+++ src/Book.js
@@ -27,7 +27,7 @@ class Book extends Component {
             <select
               onChange={ this.onShelfChange }
               defaultValue={ book.shelf || 'none' }>
-              <option value="none" disabled>Move to...</option>
+              <option value="unavailable" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
               <option value="read">Read</option>
