70c0c175316192b9d2180f53e9fd707f093cca73
Florian Feigenbutz <florian.feigenbutz@moovel.com>
2018-05-18 10:50:11 +0200

    Code Style

--- src/Book.js
+++ src/Book.js
@@ -23,7 +23,8 @@ class Book extends Component {
           <div className="book-cover" style={ coverStyle }></div>
           <div className="book-shelf-changer">
             <select
-              defaultValue={book.shelf}>
+              onChange={ this.onShelfChange }
+              defaultValue={ book.shelf }>
               <option value="none" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
