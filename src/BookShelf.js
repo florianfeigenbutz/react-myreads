83d3f7991cb43b297cc726ae474adc3ae52e8129
Florian Feigenbutz <florian.feigenbutz@moovel.com>
2018-05-18 11:02:44 +0200

    Displaying message if any book shelf is empty.

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -4,20 +4,26 @@ import Book from './Book';
 
 class BookShelf extends Component {
   render() {
+    const books = this.props.books
+      .filter((book) => { return !this.props.shelf || book.shelf === this.props.shelf });
+
     return (
       <div className="bookshelf-books">
+        {books.length > 0 &&
         <ol className="books-grid">
-          { this.props.books
-            .filter((book) => { return !this.props.shelf || book.shelf === this.props.shelf })
-            .map((book) => (
-              <li key={book.id}>
-                <Book
-                  book={book}
-                  onShelfChange={this.props.onShelfChange}
-                />
-              </li>
-            )) }
+          {books.map((book) => (
+            <li key={book.id}>
+              <Book
+                book={book}
+                onShelfChange={this.props.onShelfChange}
+              />
+            </li>
+          ))}
         </ol>
+        }
+        { books.length === 0 &&
+          <p>This shelf is currently empty.</p>
+        }
       </div>
     );
   }
